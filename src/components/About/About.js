d215b768f7f0034f5eb71912eda842ae641cf88c
Devopsnhc02 <126224060+Devopsnhc02@users.noreply.github.com>
2023-03-02 10:30:34 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,14 +7,14 @@ function About() {
         <div className="about-nike">
           <h2 className="about-nike-text">OUR MISSION</h2>
           <p>
-            "BRING INSPIRATION AND INNOVATION TO EVERY ATHLETE* IN THE WORLD"
+            "BRING INSPIRATION AND INNOVATION TO EVERY ATHLETE IN THE WORLD"
           </p>
         </div>
       </div>
       <div className="about-nike-part">
         <h2 className="about-nike-heading">About Nike</h2>
         <p className='about-nike-para'>
-          Our missioddn is what drives us to do everything possible to expand
+          Our mission is what drives us to do everything possible to expand
           human potential. We do that by creating groundbreadddking sport
           innovations, by making our products more sudddstainably, by building a
           creative and diverseacadcadc global team and by making a positive impact in
